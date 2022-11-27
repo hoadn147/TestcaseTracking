1003d397eee57146cbbecd684fb33a05fb997da8
hoadn147 <hoadn147@gmail.com>
2022-11-27 17:03:33 +0700

    123

--- TestcaseTrack/settings.py
+++ TestcaseTrack/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'core',
 ]
 
@@ -50,9 +51,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'TestcaseTrack.urls'
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
 
 TEMPLATES = [
     {
